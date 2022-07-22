b5402aed62b7c4ad41004f78f86262fde328cd77
Riya Bajpai <pizzariy4@gmail.com>
2022-07-22 11:36:32 -0400

    edited some code

--- YOUdemonia/Diet2 Scene.swift
+++ YOUdemonia/Diet2 Scene.swift
@@ -19,31 +19,31 @@ class Diet2_Scene: UIViewController {
         var enoughProtein = " "
     
         if Double(userFruitInput)! >= 0.5 {
-        enoughFruit = "You ate enough fruit today."
+        enoughFruit = "You ate enough fruit today. The recommended amount is at least 2 cups."
         } else {
         // Double(userFruitInput) -= 0.5 might need to add an exclamation mark after var name?
-        enoughFruit = "You need at some more cups of fruit." //maybe need an exclamation mark here too?
+        enoughFruit = "You need at some more cups of fruit. The recommended amount is at least 2 cups." //maybe need an exclamation mark here too?
         }
  
         if Double(userVegInput)! >= 2 {
-        enoughVeg = "You ate enough vegetables today."
+        enoughVeg = "You ate enough vegetables today. The recommended amount is at least 3 cups."
         } else {
         //userVegInput -= 2
-        enoughVeg = "You need some more cups of vegetables."
+        enoughVeg = "You need some more cups of vegetables. The recommended amount is at least 3 cups."
         }
  
         if Double(userGrainsInput)! >= 6 {
-        enoughGrains = "You ate enough grains today."
+        enoughGrains = "You ate enough grains today. The recommended amount is at least 5 - 9 ounces."
         } else {
          //userGrainsInput -=6 subtract user input value from 6 to determine how many more cups of grains user needs to eat, use abs function in interpolation
-        enoughGrains = "You need some more ounces of grains."
+        enoughGrains = "You need some more ounces of grains. The recommended amount is at least 5 - 9 ounces."
         }
  
         if Double(userProteinInput)! >= 5.5 {
-        enoughProtein = "You ate enough protein today."
+        enoughProtein = "You ate enough protein today. The recommended amount is 5 - 6.5 ounces."
         } else {
         //Double(userProteinInput) -=5.5 //might need to add exclamation mark here
-        enoughProtein = "You need some more cups of protein."
+        enoughProtein = "You need some more cups of protein. The recommended amount is 5 - 6.5 ounces."
         }
         
         
